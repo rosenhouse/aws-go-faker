aab16f7406fc635f5909b5d02834669ea839a3e3
Gabe Rosenhouse <rosenhouse@users.noreply.github.com>
2015-11-29 12:17:05 -0800

    Update godoc

--- faker.go
+++ faker.go
@@ -1,32 +1,20 @@
 // Package awsfaker supports the creation of test doubles for AWS APIs.
 //
-// Integration testing of applications that use AWS can be difficult.
-// A test suite that interacts with a live AWS account will provide good
-// test coverage, but may be slow and expensive.
-//
-// An alternative is to create a test double or "fake" of the AWS APIs that
-// your application uses.  The fake boots an HTTP server that stands in for
-// the real AWS endpoints, recording requests and providing arbitrary responses.
-//
-// This package provides a generic HTTP handler that can form the front-end
-// of a test double (mock, fake or stub) for an AWS API.  To use it in tests,
-// build a "backend" that implements the subset of the AWS API used by your code.
+// To use, build a "backend" that implements the subset of the AWS API used by your code.
 // Each API call should be implemented as a backend method like this
-//			func (b *MyBackend) SomeAction(input *service.SomeActionInput) (*service.SomeActionOutput, error)
+//	func (b *MyBackend) SomeAction(input *service.SomeActionInput) (*service.SomeActionOutput, error)
 // Then initialize an HTTP server
-//			myBackend := &MyBackend{ ... }
-//			fakeServer := httptest.NewServer(awsfaker.New(myBackend))
+//	myBackend := &MyBackend{ ... }
+//	fakeServer := httptest.NewServer(awsfaker.New(myBackend))
 // Finally, initialize your code under test, overriding the default AWS endpoint
 // to instead use your fake:
-//      app := myapp.App{ AWSOverride: fakeServer.URL }
-//      app.Run()
+//	app := myapp.App{ AWSOverride: fakeServer.URL }
+//	app.Run()
 //
-// The method signatures of the backend exactly match those of the service
+// The method signatures of the backend match those of the service
 // interfaces of the package github.com/aws/aws-sdk-go
-//
 // For example, a complete implementation of AWS CloudFormation would match the
 // CloudFormationAPI interface here: https://godoc.org/github.com/aws/aws-sdk-go/service/cloudformation/cloudformationiface
-//
 // But your backend need only implement those methods used by your code under test.
 package awsfaker
 
