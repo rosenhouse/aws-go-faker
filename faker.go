4f8c53472f8ac6c0d4b70472dc329c47386eb27f
Gabe Rosenhouse <rosenhouse@gmail.com>
2015-11-29 23:35:05 -0800

    Update godoc to reflect single-backend per handler

--- faker.go
+++ faker.go
@@ -1,14 +1,15 @@
 // Package awsfaker supports the creation of test doubles for AWS APIs.
 //
-// To use, build a "backend" that implements the subset of the AWS API used by your code.
+// To use, build a "backend" for each AWS service that you use.
+// The backend should implement the subset of that API that you need.
 // Each API call should be implemented as a backend method like this
 //	func (b *MyBackend) SomeAction(input *service.SomeActionInput) (*service.SomeActionOutput, error)
-// Then initialize an HTTP server
+// Then initialize an HTTP server for each backend
 //	myBackend := &MyBackend{ ... }
 //	fakeServer := httptest.NewServer(awsfaker.New(myBackend))
 // Finally, initialize your code under test, overriding the default AWS endpoint
 // to instead use your fake:
-//	app := myapp.App{ AWSOverride: fakeServer.URL }
+//	app := myapp.App{ AWSEndpointOverride: fakeServer.URL }
 //	app.Run()
 //
 // The method signatures of the backend match those of the service
@@ -26,13 +27,14 @@ import (
 )
 
 // New returns a new FakeHandler that will dispatch incoming requests to
-// one or more fake service backends given as arguments.
+// the given service backend.
 //
-// Each service backend should represent an AWS service, e.g. EC2 or CloudFormation.
+// A service backend should represent one AWS service, e.g. EC2
 // A backend should implement some or all of the actions of the service.
 // The methods on the backend should have signatures like
-//			func (b *MyBackend) SomeAction(input *service.SomeActionInput) (*service.SomeActionOutput, error)
+//	func (b *MyBackend) SomeAction(input *service.SomeActionInput) (*service.SomeActionOutput, error)
 // where the input and output types are those in github.com/aws/aws-sdk-go
+// When returning an error from a backend method, use the ErrorResponse type.
 func New(serviceBackend interface{}) http.Handler {
 	return query.New(serviceBackend)
 }
